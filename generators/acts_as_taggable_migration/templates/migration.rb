991a71ec42e073f5690197589d964612890b527c
Dmitriy Timokhin <avanie@gmail.com>
2008-07-11 18:40:58 +0400

    Подчищеная миграция.

--- generators/acts_as_taggable_migration/templates/migration.rb
+++ generators/acts_as_taggable_migration/templates/migration.rb
@@ -15,6 +15,21 @@ class ActsAsTaggableMigration < ActiveRecord::Migration
       t.column :created_at, :datetime
     end
     
+    create_table :tags_hierarchy, :id => false do |t|
+      t.column :tag_id, :integer
+      t.column :child_id, :integer
+    end
+
+    create_table :tags_transitive_hierarchy, :id => false do |t|
+      t.column :tag_id, :integer
+      t.column :child_id, :integer
+    end
+
+    create_table :tags_synonyms, :id => false do |t|
+      t.column :tag_id, :integer
+      t.column :synonym_id, :integer
+    end      
+    
     add_index :taggings, :tag_id
     add_index :taggings, [:taggable_id, :taggable_type]
   end
